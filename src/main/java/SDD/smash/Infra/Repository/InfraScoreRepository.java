231fa95c4b2d981cafe38a2db4be9b36b1f6f9ca
LeeHyunWoo02 <hyoun0824@naver.com>
2025-10-29 23:24:55 +0900

    chore : Add JPA Repository

---
+++ src/main/java/SDD/smash/Infra/Repository/InfraScoreRepository.java
@@ -0,0 +1,9 @@
+package SDD.smash.Infra.Repository;
+
+import SDD.smash.Infra.Entity.InfraScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InfraScoreRepository extends JpaRepository<InfraScore,String> {
+}
