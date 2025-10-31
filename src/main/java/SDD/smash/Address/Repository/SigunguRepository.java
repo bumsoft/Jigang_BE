0a7c1a464393552597c31ace7cf3bed0d7f35ae0
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-10-31 19:24:16 +0900

    feat : 시군구 코드 검증 서비스

--- src/main/java/SDD/smash/Address/Repository/SigunguRepository.java
+++ src/main/java/SDD/smash/Address/Repository/SigunguRepository.java
@@ -1,11 +1,20 @@
 package SDD.smash.Address.Repository;
 
+import SDD.smash.Address.Dto.SigunguCodeDTO;
 import SDD.smash.Address.Entity.Sigungu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SigunguRepository extends JpaRepository<Sigungu,String> {
     Sigungu findBySigunguCode(String sigunguCode);
+
+    @Query("SELECT new SDD.smash.Address.Dto.SigunguCodeDTO(s.sigunguCode) FROM Sigungu s")
+    List<SigunguCodeDTO> findAllSigunguCodes();
+
+    boolean existsBySigunguCode(String sigunguCode);
 
 }
