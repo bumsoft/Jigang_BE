4dba711f21a4b7a8528f1d0714f54021590f8656
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-10-31 19:24:16 +0900

    feat : 시군구 코드 검증 서비스

---
+++ src/main/java/SDD/smash/Address/Dto/SigunguCodeDTO.java
@@ -0,0 +1,10 @@
+package SDD.smash.Address.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SigunguCodeDTO {
+    private String sigunguCode;
+}
