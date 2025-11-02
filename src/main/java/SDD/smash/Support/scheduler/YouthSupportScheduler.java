f26c5560a3086c6799981ee006cbb3d877a63862
LeeHyunWoo02 <hyoun0824@naver.com>
2025-11-03 02:06:31 +0900

    refeact : 스케줄 시간 길게 설정

--- src/main/java/SDD/smash/Support/scheduler/YouthSupportScheduler.java
+++ src/main/java/SDD/smash/Support/scheduler/YouthSupportScheduler.java
@@ -32,7 +32,7 @@ public class YouthSupportScheduler {
      * 이후 3일 간격으로 반복 수행
      * initialDelay=0으로 컨텍스트 시작 직후 실행 => 개발시에는 불필요한 트래픽을 줄이기위해 사용x
      */
-    @Scheduled(initialDelay =0,fixedDelayString = "#{T(java.time.Duration).ofDays(3).toMillis()}")
+    @Scheduled(initialDelay =1111111110,fixedDelayString = "#{T(java.time.Duration).ofDays(3).toMillis()}")
     public void runJob()
     {
         long started = System.currentTimeMillis();
