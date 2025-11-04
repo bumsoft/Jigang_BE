23baccb16d9051c7822f19cb725dd6dbe857ef3b
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-11-04 12:53:34 +0900

    fix : 잘못된 import 수정

--- src/main/java/SDD/smash/Address/Repository/PopulationRepository.java
+++ src/main/java/SDD/smash/Address/Repository/PopulationRepository.java
@@ -1,9 +1,9 @@
 package SDD.smash.Address.Repository;
 
 import SDD.smash.Address.Entity.Population;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
